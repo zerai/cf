4833ae1c459051df9ca9a223be8e5f0668a7aad5
zero <teclaizerai@gmail.com>
2022-09-15 13:51:22 +0200

    [composer] added gesdinet/jwt-refresh-token-bundle.

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
