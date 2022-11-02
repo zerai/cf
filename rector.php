2eee9b27b10e3b826fb4e622ef163b78426ea01f
zerai <teclaizerai@gmail.com>
2022-11-02 18:43:17 +0100

    [Tools] update rector config.

--- rector.php
+++ rector.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\Core\ValueObject\PhpVersion;
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Symfony\Set\SymfonySetList;
 
@@ -17,12 +20,35 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__ . '/_vehiclereporting/tests',
     ]);
 
+    $rectorConfig->phpVersion(PhpVersion::PHP_81);
+    $rectorConfig->symfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
+
     // register a single rule
     $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
 
     // define sets of rules
     $rectorConfig->sets([
+        /**
+         * PHP
+         */
         LevelSetList::UP_TO_PHP_81,
-        SymfonySetList::SYMFONY_60
+
+        /**
+         * SYMFONY
+         */
+        SymfonySetList::SYMFONY_60,
+        SymfonySetList::SYMFONY_CODE_QUALITY,
+
+        /**
+         * DOCTRINE
+         */
+        DoctrineSetList::DOCTRINE_DBAL_30,
+        DoctrineSetList::DOCTRINE_ORM_29,
+        DoctrineSetList::DOCTRINE_CODE_QUALITY,
+
+        /**
+         * PHPUNIT
+         */
+        PHPUnitSetList::PHPUNIT_91,
     ]);
 };
