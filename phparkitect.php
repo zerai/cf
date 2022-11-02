268e76dd163feccbeaba0a2a8efdf95eed054b0a
zerai <teclaizerai@gmail.com>
2022-11-02 16:44:46 +0100

    Fix header.

--- phparkitect.php
+++ phparkitect.php
@@ -1,5 +1,18 @@
 <?php declare(strict_types=1);
 
+/*
+ * This file is part of the zerai/cf application
+ *
+ * @copyright (c) Zerai Teclai <teclaizerai@googlemail.com>.
+ * @copyright (c) Francesca Bonadonna <francescabonadonna@googlemail.com>.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * {@link https://github.com/zerai/cf/graphs/contributors developer} and is licensed under the MIT license.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Arkitect\ClassSet;
 use Arkitect\CLI\Config;
 use Arkitect\RuleBuilders\Architecture\Architecture;
